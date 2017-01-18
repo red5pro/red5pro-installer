9d14650fd7973db73d0b4b2ef9902ae28e1e244d
rajdeeprath <rajdeeprath@gmail.com>
2017-01-18 19:02:27 +0530

    Red5pro Windows Service
    + Windows service written in c#.NET
    + Starts / Stops Red5 using standard batch files
    + Added instructiosn on requirements
    + Added instructiosn on usage

---
+++ Windows/service/README.md
@@ -0,0 +1,91 @@
+# Red5 Windows Service
+
+
+
+## INTRODUCTION
+---
+
+This artifact is a windows service for Red5 / Red5pro developed / designed in visual studio 2015. Thsi can be used to register Red5pro as a standard service in windows.
+
+The service can then be configured and started / stopped using toosl built into windows. The program is developed in Windows 10 Pro, but can run on any modern windows desktop / server running Microsoft Dot Net.
+
+
+## REQUIREMENTS
+---
+
+The first requirement the Red5 service is that your `RED5_HOME` must be set and should point to your Red5 home folder where your red5.bat and red5-shutdown.bat are located.
+
+
+The second vital requirement for installing this service on windows is the `installutil.exe` utility which is provided with Microsoft.Net runtimes and Visual Studio. 
+
+Most windows systems that Red5pro is designed to run on already have some version of Dot Net installed. If a particular system does not have Dot net or has a version older than `3.5`, You can always install version `4.5` or newer the latest runtiem from its [official page](https://www.microsoft.com/en-in/download/details.aspx?id=42642)
+
+__For more information please visit the following links:__
+
+* https://msdn.microsoft.com/en-us/library/8z6watww(v=vs.110).aspx
+* https://blogs.msdn.microsoft.com/astebner/2007/03/14/mailbag-what-version-of-the-net-framework-is-included-in-what-version-of-the-os/
+
+
+## USAGE
+---
+
+__LOCATE InstallUtil.exe__
+
+* Browse to your Dotnet runtime folder :
+32 BIT : C:\Windows\Microsoft.NET\Framework
+64 BIT : C:\Windows\Microsoft.NET\Framework64
+
+* Browse into your appropriate Dotnet version folder
+>> ex: C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+__This is where the InstallUtil.exe utility is located. Note this path.__
+
+---
+
+* Open a command prompt shell on windows with administrative rights. ( Right click -> Run as Administrator )
+
+* Navigate to the location of our service - `Red5Service.exe`, in commandline
+
+---
+
+__INSTALL SERVICE:__
+
+
+`C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil Red5Service.exe`
+[ Where installutil is sourced from the DotNet runtime folder ]
+
+
+__Once service si installed you can see it in the windows services list from `Computer Management -> Services`__
+
+---
+
+__UNINSTALL SERVICE:__
+
+`C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil /U Red5Service.exe`
+[ Where installutil is sourced from the DotNet runtime folder ]
+
+---
+
+__START SERVICE:__
+
+`net start "Red5"`
+
+---
+
+__STOP SERVICE:__
+
+`net stop "Red5"`
+
+---
+
+
+__CONFIGURE SERVICE AUTOSTART:__
+
+`sc config "Red5" start=auto`
+
+---
+
+
+__CONFIGURE SERVICE MANUAL:__
+
+`sc config "Red5" start=demand`
---
+++ Windows/service/Red5Service/Red5Service/Service1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Red5Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        String RED5_HOME = "C:\\Users\\Rajdeep\\Downloads\\red5-server-1.0.8-RELEASE";
+        Process startRed5;
+        Process stopRed5;
+
+        int PID;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            RED5_HOME = System.Environment.GetEnvironmentVariable("RED5_HOME");
+            bool home_exists = System.IO.Directory.Exists(RED5_HOME);
+            
+            if (home_exists)
+            {
+                startRed5 = new Process(); // Declare New Process
+                startRed5.StartInfo.WorkingDirectory = RED5_HOME;
+                startRed5.StartInfo.FileName = "red5.bat";
+                startRed5.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startRed5.StartInfo.CreateNoWindow = true;
+                startRed5.Start();
+
+                PID = startRed5.Id;
+
+                string lines = PID.ToString();
+                System.IO.StreamWriter file = new System.IO.StreamWriter(RED5_HOME + "\\PID.txt");
+                file.WriteLine(lines);
+                file.Close();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            RED5_HOME = System.Environment.GetEnvironmentVariable("RED5_HOME");
+            bool home_exists = System.IO.Directory.Exists(RED5_HOME);
+
+
+            if (home_exists)
+            {
+                /******************************************/
+                /* First try to kill red5 process */
+
+                try
+                {
+                    if (startRed5 != null)
+                    {
+                        startRed5.CloseMainWindow();
+                        startRed5.Kill();
+                    }
+                    else
+                    {
+                        Process.GetProcessById(PID).Kill();
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    string lines = e.Message;
+                    System.IO.StreamWriter file = new System.IO.StreamWriter(RED5_HOME + "\\service_error.log");
+                    file.WriteLine(lines);
+                    file.Close();
+                }
+                finally
+                {
+                    startRed5 = null;
+                }
+
+
+                /******************************************/
+                /* Try to run shutdown script */
+
+                stopRed5 = new Process();
+                stopRed5.StartInfo.WorkingDirectory = RED5_HOME;
+                stopRed5.StartInfo.FileName = "red5-shutdown.bat";
+                stopRed5.StartInfo.Arguments = "force";
+                stopRed5.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                stopRed5.StartInfo.CreateNoWindow = true;
+                stopRed5.Start();
+            }
+        }
+    }
+}
